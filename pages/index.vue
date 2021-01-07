179a112392f02fb96f96d3af046dd39fb3618170
Naokazu Terada <naokazu.terada@gmail.com>
2021-01-07 18:46:19 +0900

    一旦コメントアウト

--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,17 @@
 </template>
 
 <script>
+// https://sheets.googleapis.com/v4/spreadsheets/1lu3DbgrhnZYCKT7KuwaZB0Y9VnMH-Vwxf-y2BstPLOM/?key=AIzaSyDcC0YMmCcMid6GjWbfQYFm314mQZ9f-WY&includeGridData=true
 export default {
-  async asyncData({ $axios }) {
-    const spreadsheetId = '1lu3DbgrhnZYCKT7KuwaZB0Y9VnMH-Vwxf-y2BstPLOM'
-    const yourAPIKey = 'AIzaSyDcC0YMmCcMid6GjWbfQYFm314mQZ9f-WY'
-    console.log('---------------')
-    // eslint-disable-next-line
-    const res = await $axios.$get(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/?key=${yourAPIKey}&includeGridData=true`)
-    console.log(res)
-    return { res }
-  },
+  // async asyncData({ $axios }) {
+  //   const spreadsheetId = '1lu3DbgrhnZYCKT7KuwaZB0Y9VnMH-Vwxf-y2BstPLOM'
+  //   const yourAPIKey = 'AIzaSyDcC0YMmCcMid6GjWbfQYFm314mQZ9f-WY'
+  //   console.log('---------------')
+  //   // eslint-disable-next-line
+  //   const res = await $axios.$get(`https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/?key=${yourAPIKey}&includeGridData=true`)
+  //   console.log(res)
+  //   return { res }
+  // },
   mounted() {
     console.log('mounted')
   },
