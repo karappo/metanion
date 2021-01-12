7bf634569c3f27c4b2940e525f5e14cdf359e8b6
Naokazu Terada <naokazu.terada@gmail.com>
2021-01-12 13:18:34 +0900

    一旦ページにわけた

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,20 +1,29 @@
 <template lang="pug">
-div
-  ul
-    li(v-for="sheet in gss.sheets")
-      nuxt-link(:to="`/${sheet.properties.sheetId}`") {{ sheet.properties.title }}
+table(v-if="sheet")
+  tr(v-for="row in sheet")
+    td(v-for="(col, idx) in row.values" v-if="idx != 0")
+      | {{ col.formattedValue }}
 </template>
 
 <script>
+import _find from 'lodash/find'
+
 const SPREADSHEET_ID = '1lu3DbgrhnZYCKT7KuwaZB0Y9VnMH-Vwxf-y2BstPLOM'
 const API_KEY = 'AIzaSyDcC0YMmCcMid6GjWbfQYFm314mQZ9f-WY'
 
 export default {
-  watchQuery: ['sheetId'],
-  async asyncData({ $axios }) {
+  async asyncData({ $axios, params, redirect }) {
     // eslint-disable-next-line
     const gss = await $axios.$get(`https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/?key=${API_KEY}&includeGridData=true`)
-    return { gss }
+    if (gss) {
+      const sheet = _find(gss.sheets, {
+        properties: { sheetId: params.id * 1 }
+      }).data[0].rowData
+
+      return { sheet }
+    } else {
+      redirect('/')
+    }
   },
   head() {
     return {
