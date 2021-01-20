f48aba6b07393648c3d473b0f0d345e786b01dd4
Naokazu Terada <naokazu.terada@gmail.com>
2021-01-20 11:33:07 +0900

    Redirect to first sentence page when access to sheet top

--- pages/_sheet/index.vue
+++ pages/_sheet/index.vue
@@ -1,20 +1,7 @@
-<template lang="pug">
-table
-  tr(v-for="row in $store.state.sheet")
-    td(v-for="(col, idx) in row.values" :key="idx" v-if="idx != 0")
-      | {{ col.formattedValue }}
-</template>
-
-<style lang="sass">
-table
-  td
-    background-color: #eee
-</style>
-
 <script>
 export default {
-  mounted() {
-    this.$store.commit('sheetId', this.$route.params.sheet)
+  fetch({ params, redirect }) {
+    redirect(301, `/${params.sheet}/文章1/`)
   }
 }
 </script>
