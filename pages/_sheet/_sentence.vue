f3ed347acbb4c13f873219dd5cb8797e333c2a9a
Naokazu Terada <naokazu.terada@gmail.com>
2021-01-20 13:50:46 +0900

    Avoid warning: [vue-router] Duplicate named routes definition: { name: "sheet", path: "/:sheet/" }

--- pages/_sheet/_sentence.vue
+++ pages/_sheet/_sentence.vue
@@ -1,28 +1,43 @@
 <template lang="pug">
-div
-  h2 {{ this.$route.params.sentence }}の回答の変化
-  table.answers(v-if="answers()")
-    thead
-      tr
-        th Before
-        th →
-        th After
-    tbody
-      tr
-        td
-        td 賛成
-        td
-      tr(v-for="hyouka in ['2', '1', '0', '-1', '-2']")
-        td.gray {{ answers().before[hyouka] || 0 }}
-        td {{ 0 < hyouka ? `+${hyouka}` : hyouka }}
-        td.gray {{ answers().after[hyouka] || 0 }}
-      tr
-        td
-        td 反対
-        td
+.content
+  div
+    h2 {{ this.$route.params.sentence }}の回答の変化
+    table.answers(v-if="answers()")
+      thead
+        tr
+          th Before
+          th →
+          th After
+      tbody
+        tr
+          td
+          td 賛成
+          td
+        tr(v-for="hyouka in ['2', '1', '0', '-1', '-2']")
+          td.gray {{ answers().before[hyouka] || 0 }}
+          td {{ 0 < hyouka ? `+${hyouka}` : hyouka }}
+          td.gray {{ answers().after[hyouka] || 0 }}
+        tr
+          td
+          td 反対
+          td
+  .sentences
+    nuxt-link(
+      v-for="(v, k) in $store.state.answersBySentence"
+      :key="k"
+      :to="`/${$store.state.sheetId}/${k}/`"
+      :class="{current: $route.fullPath === `/${$store.state.sheetId}/${k}/`}"
+    ) {{ k }}
 </template>
 
 <style lang="sass">
+.content
+  width: 100%
+  min-height: calc(100vh - 60px)
+  display: flex
+  justify-content: center
+  align-items: center
+  position: relative
 h2
   text-align: center
 table.answers
@@ -40,6 +55,14 @@ table.answers
   tr:first-child
     td
       background-color: white
+.sentences
+  text-align: center
+  position: absolute
+  bottom: 30px
+  a + a
+    margin-left: 15px
+  a.current
+    border-color: #546fff
 </style>
 
 <script>
