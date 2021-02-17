542887deb3a07a69d24046b67a4224bd4b7632aa
Naokazu Terada <naokazu.terada@gmail.com>
2021-02-17 14:25:03 +0900

    フッターのスタイル

--- pages/_sheet/_sentence.vue
+++ pages/_sheet/_sentence.vue
@@ -33,20 +33,23 @@
           .row(v-for="p in points")
             .dot(v-for="i in (answers().count.after[p] || 0)")
   footer
-    nuxt-link(to="/").logotype
-      Logotype
-    .sentences
-      nuxt-link(
-        v-for="(v, k, i) in $store.state.answers"
-        :key="k"
-        :to="`/${$store.state.sheetId}/${k}/`"
-        :class="{current: decodeURI($route.fullPath) === `/${$route.params.sheet}/${k}/`}"
-      )
-        span {{ i + 1 }}
-    a.toggleButton(@click="toggle")
-      IconClose(v-if="showTransform")
-      IconOpen(v-else)
-    ExternalLink.gssLink(v-if="sheet()" :href="gssLinkURI()" targe='_blank') {{ sheet().properties.title }}
+    .left
+      nuxt-link(to="/").logotype
+        Logotype
+    .center
+      .pages
+        nuxt-link(
+          v-for="(v, k, i) in $store.state.answers"
+          :key="k"
+          :to="`/${$store.state.sheetId}/${k}/`"
+          :class="{current: decodeURI($route.fullPath) === `/${$route.params.sheet}/${k}/`}"
+        )
+          span {{ i + 1 }}
+    .right
+      a.toggleButton(@click="toggle")
+        IconClose(v-if="showTransform")
+        IconOpen(v-else)
+      ExternalLink.gssLink(v-if="sheet()" :href="gssLinkURI()" targe='_blank') {{ sheet().properties.title }}
 </template>
 
 <style lang="sass" scoped>
@@ -185,46 +188,85 @@ h1
     background-color: #999999
     border-radius: 7px
 footer
+  $center_width: 756px // 84 * 9 = 756
+
   height: $footer_inner_height
   border-top: 1px solid #CCCCCC
   margin: 0
   display: flex
-.logotype
-  margin-left: 56px
-  display: flex
-  align-items: center
-.sentences
-  text-align: center
-  margin-left: auto
-  margin-right: auto
-  display: flex
-  align-items: center
-  a
-    span
+  .left
+    display: flex
+    align-items: center
+    width: 100%
+    margin-left: 56px
+    .logotype
       display: flex
-      justify-content: center
       align-items: center
-      width: 84px
-      height: 24px
-    &.current
-      span
-        background-color: #999999
-        color: white
-  a + a
-    margin-left: 15px
-.toggleButton
-  display: flex
-  align-items: center
-  justify-content: center
-  width: $footer_inner_height
-  height: $footer_inner_height
-  cursor: pointer
-.gssLink
-  display: flex
-  align-items: center
-  font-size: 16px
-  margin-left: 130px
-  margin-right: 56px
+  .center
+    // background: green
+    display: flex
+    align-items: center
+    width: $center_width
+    text-align: center
+    margin-left: auto
+    margin-right: auto
+    .pages
+      display: flex
+      align-items: center
+      a
+        display: flex
+        align-items: center
+        &::before,
+        &::after
+          top: calc(50% - 6px)
+          content: ''
+          display: block
+          height: 12px
+          width: 1px
+          background-color: #BFBFBF
+        &:first-of-type::before
+          left: 0
+        &::after
+          right: 0
+        &.current::before,
+        &.current::after
+          background-color: white
+        span
+          display: flex
+          justify-content: center
+          align-items: center
+          width: 82px
+          height: 24px
+        &.current
+          z-index: 1
+          span
+            background-color: #999999
+            color: white
+      a + a
+        margin-left: -1px
+  .right
+    display: flex
+    align-items: center
+    margin-right: 56px
+    width: 100%
+    .toggleButton
+      display: flex
+      align-items: center
+      justify-content: center
+      width: 100%
+      min-width: $footer_inner_height
+      cursor: pointer
+      margin: 0 auto
+      flex-grow: 1
+    .gssLink
+      display: flex
+      align-items: center
+      font-size: 16px
+      justify-content: flex-end
+      width: auto
+      flex-grow: 3
+      text-overflow: ellipsis
+      white-space: nowrap
 </style>
 
 <script>
